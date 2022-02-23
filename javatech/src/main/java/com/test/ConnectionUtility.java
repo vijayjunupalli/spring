859d9635a19720c4e5490f0fcd42533997a2f1c8
vijay <vijay.junupalli@gmail.com>
2022-02-23 15:06:27 -0500

    for jenkins demo

--- javatech/src/main/java/com/test/ConnectionUtility.java
+++ javatech/src/main/java/com/test/ConnectionUtility.java
@@ -25,6 +25,7 @@ public class ConnectionUtility {
 			String sql = "insert into userDetails(name, password, email) values('" + userName + "','" + password + "','"
 					+ email + "')";
 			int result = statment.executeUpdate(sql);
+			System.out.println("execute result....");
 
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
